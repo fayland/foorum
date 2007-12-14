80ff9fa6f8597014aa0c30ab11232eae6ec33404
fayland <fayland@gmail.com>
2007-12-14 11:30:14 +0000

    add File::HomeDir support

--- bin/cron/chart.pl
+++ bin/cron/chart.pl
@@ -5,9 +5,10 @@ use warnings;
 
 # for both Linux/Win32
 my $has_proc_pid_file = eval "use Proc::PID::File; 1;";
-if ($has_proc_pid_file) {
+my $has_home_dir = eval "use File::HomeDir; 1;";
+if ($has_proc_pid_file and $has_home_dir) {
     # If already running, then exit
-    if (Proc::PID::File->running()) {
+    if (Proc::PID::File->running( { dir => File::HomeDir->my_home } )) {
         exit(0);
     }
 }
