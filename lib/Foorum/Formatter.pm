d1b2a5b3f6ddcb18d7382aee3a2ab1fc82d86fbb
fayland <fayland@gmail.com>
2007-12-17 14:18:46 +0000

    simple wiki format test case.

--- lib/Foorum/Formatter.pm
+++ lib/Foorum/Formatter.pm
@@ -3,13 +3,16 @@ package Foorum::Formatter;
 use strict;
 use warnings;
 use base 'Exporter';
-use vars qw/@EXPORT_OK $has_text_textile $has_ubb_code $VERSION/;
+use vars qw/@EXPORT_OK $VERSION/;
 @EXPORT_OK = qw/
     filter_format
     /;
 $VERSION = '0.01'; # version
+
+use vars qw/$has_text_textile $has_ubb_code $has_text_wiki/;
 $has_text_textile = eval "use Text::Textile; 1;";
 $has_ubb_code     = eval "use Foorum::Formatter::BBCode; 1;";
+$has_text_wiki    = eval "use Text::WikiFormat; 1;";
 
 sub filter_format {
     my ( $text, $params ) = @_;
@@ -26,8 +29,16 @@ sub filter_format {
             }
         );
         $text = $formatter->parse($text);
+    } elsif ( $format eq 'wiki' and $has_text_wiki) {
+        $text =~ s/&/&amp;/gs;
+        $text =~ s/>/&gt;/gs;
+        $text =~ s/</&lt;/gs;
+        my %tags = %Text::WikiFormat::tags;
+        my %opts = ( extended => 1, absolute_links => 1 );
+        $text = Text::WikiFormat::format($text, \%tags, \%opts);
     } else {
-        $text =~ s|<|&lt;|gs;         # no_html
+        $text =~ s/&/&amp;/gs;   # no_html
+        $text =~ s|<|&lt;|gs;
         $text =~ s|>|&gt;|gs;
         $text =~ s|\n|<br />\n|gs;    # linebreaks
     }
@@ -53,7 +64,11 @@ Foorum::Formatter - format content for Foorum
   print $html;
   # <img src="/static/images/bbcode/emot/inlove.gif"> <span style="font-weight:bold">Test</span> <span style="font-size:14px">dsadsad</span> <a href="http://fayland/">da</a>
 
-=head2 AUTHOR
+=head1 SEE ALSO
+
+L<HTML::BBCode>, L<Text::Textile>, L<Text::WikiFormat>
+
+=head1 AUTHOR
 
 Fayland Lam <fayland@gmail.com>
 
---
+++ t/formatter/wiki.t
@@ -0,0 +1,46 @@
+use strict;
+use warnings;
+use Test::More;
+
+BEGIN {
+
+    eval { require Text::WikiFormat }
+        or plan skip_all =>
+        "Text::WikiFormat is required for this test";
+    
+    plan tests => 3;
+}
+
+use Foorum::Formatter qw/filter_format/;
+
+my $text = <<TEXT;
+= my interesting text =
+
+ANormalLink
+[let the Sun shine|AnExtendedLink]
+
+== my interesting lists ==
+
+    * unordered one
+    * unordered two
+
+    1. ordered one
+    2. ordered two
+        a. nested one
+        b. nested two
+
+    code one
+    code two
+
+The first line of a '''normal''' paragraph.
+The second line of a normal paragraph.  Whee.
+such as http://www.cpan.org/
+TEXT
+
+my $html = filter_format($text, { format => 'wiki' } );
+
+like($html, qr/ol/,   '*,* OK');
+like($html, qr/ul/,   '1,2 OK');
+like($html, qr/\<a href=/, 'extended => 1, absolute_links => 1 OK');
+
+#diag($html);
